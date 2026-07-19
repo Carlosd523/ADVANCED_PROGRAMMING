b6802dd985dc20d92568554c660f0c130cd88000
Carlos Angulo <cdangulo08@gmail.com>
2026-07-19 16:33:09 -0600

    Finalización - Ejercicio 01

--- LAB2+_+CARLOS+_+ANGULO+_+1250826/Ejercicio 01/Ejercicio 01/Ejercicio 01.cpp
+++ LAB2+_+CARLOS+_+ANGULO+_+1250826/Ejercicio 01/Ejercicio 01/Ejercicio 01.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// Clase Base - Material Bibliográfico
 class MaterialBibliografico {
 private:
     string nombre;
@@ -20,6 +21,10 @@ public:
     }
 
     void mostrarInformacion() {
+        cout << "Información de " << nombre
+            << "\nCódigo; " << codigo
+            << "\nAño de publicación: " << anioPublicacion
+            << "\nDisponible: " + getDisponibilidad() << endl;
     }
 
     void prestarMaterial() {
@@ -36,7 +41,7 @@ public:
             throw runtime_error("El material bibliográfico ya ha sido prestado");
         }
         else {
-            cout << "El material será prestado por: " + to_string(diasPrestamo) << endl;
+            cout << "El material será prestado por: " + to_string(diasPrestamo) << " días." << endl;
             disponible = false;
         }
     }
@@ -56,14 +61,6 @@ public:
         return nombre;
     }
 
-    string getCodigo() {
-        return codigo;
-    }
-
-    int getAnioPublicacion() {
-        return anioPublicacion;
-    }
-
     string getDisponibilidad() {
         if (disponible) {
             return "Sí";
@@ -76,8 +73,12 @@ public:
     virtual void mostrarDetalle() {
     }
 
+    virtual ~MaterialBibliografico(){
+        cout << "Objeto destruido" << endl;
+    }
 };
 
+// Clase Derivada 02 - Libro
 class Libro : public MaterialBibliografico {
 private:
     string autor;
@@ -89,11 +90,7 @@ public:
         : MaterialBibliografico(n, c, aP, d), autor(autor), numeroPaginas(numeroPaginas) {}
 
     void mostrarDetalle() override {
-        cout << "Información de " << getNombre()
-            << "\nCódigo; " << getCodigo()
-            << "\nAño de publicación: " << getAnioPublicacion()
-            << "\nDisponible: " + getDisponibilidad()
-            << "\nAutor: " << autor
+        cout << "\nAutor: " << autor
             << "\nNúmero de páginas: " << numeroPaginas << endl;
     }
 
@@ -101,8 +98,10 @@ public:
         double tiempoLectura = (numeroPaginas * 2) / 60;
         cout << "El tiempo aproximado de lectura es de: " + to_string(tiempoLectura) << endl;
     }
+
 };
 
+// Clase Derivada 02 - Revista
 class Revista : public MaterialBibliografico {
 private:
     int numeroEdicion;
@@ -113,11 +112,7 @@ public:
         : MaterialBibliografico(n, c, aP, d), numeroEdicion(numeroEdicion), categoria(categoria) {}
 
     void mostrarDetalle() override {
-        cout << "Información de " + getNombre()
-            << "\nCódigo; " + getCodigo()
-            << "\nAño de publicación: " + getAnioPublicacion()
-            << "\nDisponible: " + getDisponibilidad()
-            << "\nNúmero de edición: " + numeroEdicion
+        cout << "\nNúmero de edición: " + to_string(numeroEdicion)
             << "\nCategoría: " + categoria << endl;
     }
 
@@ -128,5 +123,29 @@ public:
 
 int main()
 {
+    // Creación de Objetos
+    Libro libro01("El Señor de los Anillos", "L001", 1954, true, "J. R. R. Tolkien", 1392);
+    Libro libro02("Las Crónicas de Narnia: El Príncipe Caspian", "L002", 1951, true, "C. S. Lweis", 280);
+
+    Revista revista01("Productos para un Salón de Belleza", "R001", 2017, true, 4, "Belleza");
+    Revista revista02("Productos para tu Cocina", "R002", 2023, true, 7, "Gastronomía");
+
+    // Utilización de prestarMaterial sin parámetros
+    libro01.prestarMaterial();
+    revista01.prestarMaterial();
+
+    // Utilización de prestarMaterial con un parámetro
+    libro02.prestarMaterial(25);
+    revista02.prestarMaterial(45);
+
+    libro01.mostrarInformacion();
+    libro02.mostrarInformacion();
+    revista01.mostrarInformacion();
+    revista02.mostrarInformacion();
+
+    libro01.mostrarDetalle();
+    libro02.mostrarDetalle();
+    revista01.mostrarDetalle();
+    revista02.mostrarDetalle();
     return 0;
 }
